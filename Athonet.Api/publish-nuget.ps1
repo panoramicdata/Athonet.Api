cee75ed6a11b20ea4d1ceb35e911878e3255726b
David Bond <david.bond@panoramicdata.com>
2020-11-10 14:52:42 +0000

    Added publish-nuget.ps1

---
+++ Athonet.Api/publish-nuget.ps1
@@ -0,0 +1,18 @@
+﻿# This script will publish to nuget using the api key in nuget-api-key.txt in the same folder.
+# The api key issued by nuget.org should ideally only have permissions to update a single package
+# with new versions.
+
+$apiKeyFilename = "nuget-api-key.txt";
+if(-not (Test-Path($apiKeyFilename))){
+	Write-Host "$apiKeyFilename does not exist"
+	exit 1;
+}
+$apiKey = Get-Content $apiKeyFilename;
+
+dotnet build -c Release
+dotnet pack -c Release
+
+$mostRecentPackage = Get-ChildItem bin\Release\*.nupkg | Sort-Object LastWriteTime | Select-Object -last 1
+Write-Host "Publishing $mostRecentPackage..."
+# If you don't have nuget.exe - download from https://www.nuget.org/downloads and place in "C:\Users\xxx\AppData\Local\Microsoft\WindowsApps"
+nuget.exe push -Source https://api.nuget.org/v3/index.json -ApiKey $apiKey "$mostRecentPackage"
