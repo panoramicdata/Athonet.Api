fac5bbc1722c1e2f1b0781f0ec82ac3cf9dd98e1
David Bond <david.bond@panoramicdata.com>
2026-08-30 01:24:41 +0100

    refactor: simplify TaiTests by using AssertTaiIsParsed method for assertions

--- Athonet.Api.Test/TaiTests.cs
+++ Athonet.Api.Test/TaiTests.cs
@@ -4,39 +4,25 @@ public class TaiTests
 {
 	[Fact]
 	public void String_Succeeds()
-	{
-		var evt = new EventDetails
-		{
-			TaiRaw = "tac-lb83.tac-hb00.tac.epc.mnc340.mcc311.3gppnetwork.org"
-		};
-
-		_ = evt.Tai.Should().BeOfType<Tai>();
-		_ = evt.Tai.Plmn.Should().Be("311340");
-		_ = evt.Tai.Tac.Should().Be(131);
-	}
+		=> AssertTaiIsParsed("tac-lb83.tac-hb00.tac.epc.mnc340.mcc311.3gppnetwork.org");
 
 	[Fact]
 	public void Tai_Succeeds()
-	{
-		var evt = new EventDetails
+		=> AssertTaiIsParsed(new Tai
 		{
-			TaiRaw = new Tai
-			{
-				Plmn = "311340",
-				Tac = 131
-			}
-		};
+			Plmn = "311340",
+			Tac = 131
+		});
 
-		_ = evt.Tai.Should().BeOfType<Tai>();
-		_ = evt.Tai.Plmn.Should().Be("311340");
-		_ = evt.Tai.Tac.Should().Be(131);
-	}
 	[Fact]
 	public void JObject_Succeeds()
+		=> AssertTaiIsParsed(JsonDocument.Parse("{ \"plmn\": \"311340\", \"tac\": 131 }").RootElement);
+
+	private static void AssertTaiIsParsed(object taiRaw)
 	{
 		var evt = new EventDetails
 		{
-			TaiRaw = JsonDocument.Parse("{ \"plmn\": \"311340\", \"tac\": 131 }").RootElement
+			TaiRaw = taiRaw
 		};
 
 		_ = evt.Tai.Should().BeOfType<Tai>();
